31c38d921743ae7e747c90acf3113fdfa2418ff0
Shane Crosby <iam@shanecrosby.com>
2023-10-27 10:38:02 +0800

    initial commit

---
+++ src/pages/functions.js
@@ -0,0 +1,54 @@
+// src/pages/functions.js
+import React from 'react';
+import Layout from '../components/layout'; // Import the layout component
+import { graphql, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+
+const IndexPage = () => {
+    const data = useStaticQuery(graphql`
+    query {
+        allFile(filter: {extension: { in: ["jpeg", "jpg", "png"] } }) {
+            nodes {
+                relativePath
+                childImageSharp {
+                    gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+                }
+            }
+        }
+        bannerImage: file(relativePath: { eq: "bruno-cervera-Gi6-m_t_W-E-unsplash.jpg" }) {
+            childImageSharp {
+                gatsbyImageData(placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
+            }
+        }
+    }
+`);
+
+    return (
+        <Layout>
+                <section>
+                        <GatsbyImage image={getImage(data.bannerImage)} alt="" className="banner-image" />
+                        <div className='intro-container'>
+                            <p className='intro-p'>
+                                Let Bella host your function. From birthday parties to wedding receptions to board meetings, we've got you covered.
+                            </p>
+                        </div>
+                        
+                        <div className="body-text">
+                            <h1 className="section-header">Facilities for hire</h1>
+                            <h2 className="section-subtitle">Function Room</h2>
+                            <p className="body-p">
+
+                            </p>
+                            <hr />
+                            <h2 className="section-subtitle">Café Space</h2>
+                            <p className="body-p">
+
+                            </p>
+                        </div>
+
+                </section>
+        </Layout>
+    );
+};
+
+export default IndexPage;
